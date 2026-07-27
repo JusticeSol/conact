86b82b7aa733ce7497a220454dcb9dcdd7e90c89
JusticeSol <justiceundie@gmail.com>
2026-07-27 03:40:22 +0100

    remove loading splash

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,7 @@ const MarketplaceApp = dynamic(
   () => import('./components/Marketplace').then(mod => ({ 
     default: mod.MarketplaceApp 
   })),
-  { 
-    ssr: false,
-    loading: () => (
-      <div style={{ 
-        background: '#09090f', 
-        minHeight: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#6b7fff',
-        fontFamily: 'sans-serif',
-        fontSize: 14,
-      }}>
-        Loading CONACT...
-      </div>
-    )
-  }
+  { ssr: false }
 )
 
 export default function Home() {
